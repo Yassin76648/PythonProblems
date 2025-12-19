db0903d94eb65ed4d2702a834ee5183073f5b05e
Yassin Sh <yassin.shaaban2004@gmail.com>
2025-12-19 15:57:34 +0200

    Implement pattern printing in sol.py
    Add pattern printing for numbers 1 to 5.

--- sol.py
+++ sol.py
@@ -7,3 +7,15 @@ else :
     result = number1 + number2
     
 print(result)
+
+# Exercise 8: Print the following pattern
+# 1 
+# 2 2 
+# 3 3 3 
+# 4 4 4 4 
+# 5 5 5 5 5
+
+for i in range(1, 6):
+    print()
+    for j in range(0, i):
+        print(i , end = "")
